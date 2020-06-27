0dff1294fdfeede86c04b07f1cd8bcb9c9b349a4
Sabyasachi Sarder <57228134+Saby053@users.noreply.github.com>
2020-06-27 22:42:24 +0530

    Update Controller.java

--- src/com/controller/Controller.java
+++ src/com/controller/Controller.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * This is Servlet implementation class Controller
+ * Servlet implementation class Controller
  */
 @WebServlet("/Controller")
 public class Controller extends HttpServlet {
